bc33bf07bfe5fa55a7f2156dd7fb8025eeddac9e
Feng Zhang <fzhang@copper.com>
2023-01-02 07:47:50 -0500

    almost complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_01_100843) do
+ActiveRecord::Schema.define(version: 2023_01_02_014349) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2023_01_01_100843) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
